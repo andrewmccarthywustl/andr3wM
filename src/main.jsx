04b33ea28abedf6f8287b2f35b7ea739357a3d9a
andrewmccarthywustl <a.g.mccarthy@wustl.edu>
2024-09-19 16:10:26 -0500

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
