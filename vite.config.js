70dcffe4524ad24238d1ca417086de7701ae5f81
andrewmccarthywustl <a.g.mccarthy@wustl.edu>
2025-02-18 20:19:34 -0600

    attempting to fix production deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   define: {
     "process.env": {},
   },
   server: {
-    host: "0.0.0.0", // This allows external access
+    host: "0.0.0.0",
   },
 });
